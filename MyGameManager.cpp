9f642678d47fd639856dcad12e9146f3275da8ce
Angelo Davis <francisdavia11@gmail.com>
2023-08-22 22:32:05 +0100

    Game manager added
    This class is incomplete but it is intended to organise communication between actors in the game mode and to remove responsibility from the player pawn to organise the level.

---
+++ MyGameManager.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameManager.h"
+#include "MyCharacter.h"
+#include "MyBed.h"
+#include "MyNoiseMaker.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AMyGameManager::AMyGameManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyGameManager::BeginPlay()
+{
+	Super::BeginPlay();
+	//Getting all the actors that need to be communicated with
+	PlayerCharacter = Cast<AMyCharacter>(UGameplayStatics::GetActorOfClass(GetWorld(), AMyCharacter::StaticClass()));
+	Bed = Cast<AMyBed>(UGameplayStatics::GetActorOfClass(GetWorld(), AMyBed::StaticClass()));
+	TArray<AActor*> FoundNoiseActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AMyNoiseMaker::StaticClass(), FoundNoiseActors);
+	for (AActor*  NoiseActor: FoundNoiseActors) {
+		FoindNoiseMakers.Add(Cast<AMyNoiseMaker>(NoiseActor));
+	}
+}
+
+// Called every frame
+void AMyGameManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ MyGameManager.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyGameManager.generated.h"
+
+
+class AMyNoiseMaker; //forward declaration makes the array below work
+
+UCLASS()
+class SUNDAYNIGHT_API AMyGameManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyGameManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	class AMyCharacter* PlayerCharacter;
+
+	class AMyBed* Bed;
+
+	TArray<AMyNoiseMaker*> FoindNoiseMakers;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+};
